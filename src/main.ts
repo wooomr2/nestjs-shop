b739b777ae7fca6a2e6b8745bd54c134ed834d86
wooomr2 <wooomr2@gmail.com>
2024-05-02 14:14:58 +0900

    helmet, version prefix 추가

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import helmet from 'helmet'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe())
-  const configService = app.get(ConfigService)
+  app.setGlobalPrefix('api/v1')
+  app.use(helmet())
 
+  const configService = app.get(ConfigService)
   await app.listen(configService.getOrThrow('PORT'))
 }
 bootstrap()
